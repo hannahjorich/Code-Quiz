3f5d9749c5dd6d6dbd523511fe87e55421170553
hannahjorich <hannah.jorich@gmail.com>
2020-07-15 22:14:24 -0600

    pseudo code

---
+++ script.js
@@ -0,0 +1,9 @@
+// how do we start? 
+
+// create a timer attached to a button with a start value of 0 
+// The timer needs to start when the user clicks start quiz 
+// when the timer starts a reverse countdown of 75 happens 
+
+// Store the users choices 
+// When user selects the right answer, textcontent "Correct!"
+// When user selects the right answer, textcontent "Wrong!"
